fddcfb1cf75855f02c04f657f7d38c249c70833e
OuSimeng <dihoshea@gmail.com>
2021-09-21 18:12:31 +0800

    init

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,7 @@ nonebot.init()
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
 nonebot.load_builtin_plugins()
-nonebot.load_plugins('plugins')
+nonebot.load_plugins('intereor/plugins')
 
 if __name__ == "__main__":
     nonebot.run()
--- src/plugins/plugin_market/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import command_find
-from . import command_jita
-from . import command_col
-from . import command_char
